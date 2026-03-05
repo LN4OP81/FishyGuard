3dc579fb17d88d86fdd1be8a731ddd6681861d62
Pranay <pranaykriplani@gmail.com>
2026-03-05 22:45:07 +0530

    Update main app structure and connect modules
    • Replaced old FishyGuardUI setup with the new PhishingUI.
    • Added the phishing detection model to the app.
    • Connected the database so predictions can be saved.
    • Added the phishing analyst feature with API key support.
    • Updated main.py to start the app and connect all parts together.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-
 import tkinter as tk
-from ui import FishyGuardUI
+from ui import PhishingUI
+from model import PhishingModel
+from database import PredictionHistory
+from analyst import PhishingAnalyst
+
+def main():
+
+    API_KEY = "FOR SECURITY PURPOSES I CANNOT LIST API KEY HERE" #But for reference, this is where your Gemini API key goes
+    
+    root = tk.Tk()
+    
+    model_engine = PhishingModel()
+    db = PredictionHistory()
+    analyst = PhishingAnalyst(api_key=API_KEY)
+    
+    app = PhishingUI(root, model_engine, db, analyst=analyst)
+    
+    root.mainloop()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = FishyGuardUI(root)
-    root.mainloop()
+    main()
